96c5bca53f157faa8d7894333c325b4b98de6a3b
ilja-jomedia <ilja@jomediainc.com>
2014-04-22 13:30:44 +0300

    stylize canvas

--- composerCanvas.directive.js
+++ composerCanvas.directive.js
@@ -1,7 +1,39 @@
-app.directive("composerCanvas", function() {
+app.directive("composerCanvas", function($window) {
+    var resizeCanvas = function(canvas) {
+        canvas[0].width = $window.innerWidth * 0.7;
+        canvas[0].height = canvas[0].width * 0.5;
+    }
+
+    var placeText = function(canvas, text) {
+        var ctx = canvas[0].getContext("2d");
+        
+        var canvasWidth = canvas[0].width;
+        var canvasHeight = canvas[0].height;
+
+        ctx.font = "1em serif";
+        ctx.textAlign = "center";
+        ctx.fillStyle = "White";
+        ctx.fillText(text, canvasWidth / 2, canvasHeight / 2);
+    }
+
     return {
         restrict: "E",
         replace: true,
         template: "<canvas></canvas>",
+        link: function (scope, elem, attrs) {
+            var initCanvas = function() {
+                resizeCanvas(elem);
+                if (attrs.text) {
+                    placeText(elem, attrs.text);
+                }
+            }
+
+            initCanvas();
+            angular.element($window).bind('resize', function() {
+                initCanvas();
+            });
+
+            
+        }
     }
 });
